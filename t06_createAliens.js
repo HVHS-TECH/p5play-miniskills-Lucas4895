c29c90f3f3a87114014b9f5c2ce83f0cf44f9d0b
Lucas4895 <23554ln@hvhs.school.nz>
2026-02-12 10:59:47 +1300

    11 february 2026
    t06_createAliens: task finished

--- t06_createAliens.js
+++ t06_createAliens.js
@@ -10,23 +10,30 @@
 function setup() {
 	console.log("setup: ");
 	randNum = random(100, 800);
+    randNumForAliens = random(3, 10);
     cnv = new Canvas(900, 900);
 	world.gravity.y = 10;
+
+
 	rectangle = new Sprite(500, 200, 100, 150, 10);
     rectangle.color = '#222';
 	rectangle.rotationSpeed = 2;
-	rectangle.bounciness = 3;
+	rectangle.bounciness = 1;
+    
     wallLH  = new Sprite(0, height/2, 15, height, 'k');
-    wallLH.color = 'black';
+    wallLH.color = "#000"
 	wallRH  = new Sprite(900, height/2, 15, height, 'k');
+    wallRH.color = "#321"
 	wallTop = new Sprite(windowWidth/2, height, windowWidth, 15, 'k')
+    wallTop.color = "#764"
 	wallBot = new Sprite(windowWidth/2, height/20, windowWidth, 15, 'k');
+    wallBot.color = "#981"
 
-    for (i = 0; i < alien; i++) {
+    for (i = 0; i < randNumForAliens; i++) {
      alien = new Sprite(randNum, randNum, 20, 20);
 	 alien.color = '#999'
-     alien.vel.x = 3;
-     alien.vel.y = 4;
+     alien.vel.x = randNumForAliens;
+     alien.vel.y = randNumForAliens;
      alien.bounciness = 1;
      alien.friction = 0;
     };
